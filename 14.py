f127c80ee6b0b6bd9c7032e66bbe6f60f1e31b36
junggri <gkgkgk6933@gmail.com>
2021-01-19 20:53:54 +0900

    210119

---
+++ 14.py
@@ -0,0 +1,32 @@
+input = [1, 2, 3, 4]
+
+
+class LinkNode:
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
+
+
+class LinkList:
+    def __init__(self):
+        self.head = None
+
+
+def makeList(input):
+    list = LinkList()
+    for i in range(len(input)):
+        node = LinkList()
+        if i == 0:
+            node.val = i
+            node.next = 'node%s+1' % i
+            list.head = node
+        elif i == len(input) - 1:
+            node.val = i
+            node.next = None
+        else:
+            node.val = i
+            node.next = 'node%s+1' % i
+        print(node)
+    
+
+a = makeList(input)
