81b60bb6db8cea0016c1eda8507f509f60fef6d2
junggri <gkgkgk6933@gmail.com>
2021-02-04 23:09:36 +0900

    210204

---
+++ 홀짝 연결리스트.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head):
+        odd = head
+        # even=head.next
+        # even_head=head.next
+        while odd and odd.next:
+            print(id(odd), id(head))
+            odd.next = odd.next.next
+
+            odd = odd.next
+
+            # even.next=even.next.next
+            # even=even.next
+        # odd.next=even_head
+        return head
